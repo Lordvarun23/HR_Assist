d01743cb3e898b24c59ad6faa65a70c7fb4034e9
varun <69851775+Lordvarun23@users.noreply.github.com>
2022-11-07 11:15:06 +0530

    Update Score.py

--- Score.py
+++ Score.py
@@ -2,6 +2,7 @@ from pdfminer.high_level import extract_text
 import re
 import nltk
 nltk.download('stopwords')
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 from textblob import TextBlob
 import spacy
